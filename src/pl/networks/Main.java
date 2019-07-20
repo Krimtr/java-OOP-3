00fb44572535b48d20540f6d68279b814f304635
Krimt <Krimt@DESKTOP-89F5LS9>
2019-07-20 13:21:50 +0200

    Working simulation

--- src/pl/networks/Main.java
+++ src/pl/networks/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello world");
+
+        Simulation simulation1 = new Simulation();
+        //Phase 1
+        simulation1.loadItems("Phase1");
+        simulation1.loadU1(simulation1.getItems());
+        simulation1.loadItems("Phase1");
+        simulation1.loadU2(simulation1.getItems());
+        System.out.println(simulation1.runSimulation());
+
+        Simulation simulation2 = new Simulation();
+        //Phase 2
+        simulation2.loadItems("Phase2");
+        simulation2.loadU1(simulation2.getItems());
+        simulation2.loadItems("Phase2");
+        simulation2.loadU2(simulation2.getItems());
+        System.out.println(simulation2.runSimulation());
+
     }
 }
--- src/pl/networks/Simulation.java
+++ src/pl/networks/Simulation.java
@@ -3,6 +3,7 @@ package pl.networks;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Simulation
@@ -37,10 +38,105 @@ public class Simulation
     public void loadU1(ArrayList<Item> items)
     {
         U1 newRocket = new U1();
-
+        int loadedItems;
+        Iterator iterator = items.iterator();
         while (!items.isEmpty())
         {
-            
+            loadedItems = 0;
+            while (iterator.hasNext())
+            {
+                Item item = (Item) iterator.next();
+                if(newRocket.canCarry(item))
+                {
+                    newRocket.carry(item);
+                    //items.remove(item);
+                    iterator.remove();
+                    loadedItems++;
+                }
+            }
+            // If no items is loaded rocket is full
+            if (loadedItems < 1)
+            {
+                loadU1(items);
+            }
+
         }
+        U1Rockets.add(newRocket);
+    }
+
+    public void loadU2(ArrayList<Item> items)
+    {
+        U2 newRocket = new U2();
+        int loadedItems;
+        Iterator iterator = items.iterator();
+        while (!items.isEmpty())
+        {
+            loadedItems = 0;
+            while (iterator.hasNext())
+            {
+                Item item = (Item) iterator.next();
+                if(newRocket.canCarry(item))
+                {
+                    newRocket.carry(item);
+                    //items.remove(item);
+                    iterator.remove();
+                    loadedItems++;
+                }
+            }
+            // If no items is loaded rocket is full
+            if (loadedItems < 1)
+            {
+                loadU2(items);
+            }
+
+        }
+        U2Rockets.add(newRocket);
+    }
+
+    public String runSimulation()
+    {
+        int U1TotalCost = 0;
+        int U2TotalCost = 0;
+        int U1RocketsUsed = 0;
+        int U2RocketsUsed = 0;
+        String result;
+
+        for (U1 rocket: this.U1Rockets)
+        {
+            do
+            {
+                U1TotalCost += rocket.getCost();
+                U1RocketsUsed++;
+            }while (!rocket.launchLand(rocket.getLaunchCrash()) || !rocket.launchLand(rocket.getLandCrash()));
+        }
+
+        for (U2 rocket: this.U2Rockets)
+        {
+            do
+            {
+                U2TotalCost += rocket.getCost();
+                U2RocketsUsed++;
+            }while (!rocket.launchLand(rocket.getLaunchCrash()) || !rocket.launchLand(rocket.getLandCrash()));
+        }
+
+        result = "Simulation results:\n" +
+                "U1 rockets total cost: " + U1TotalCost + "\nU1 rockets used: " + U1RocketsUsed + "\nMinimum U1 rockets needed: " + this.U1Rockets.size() +
+                "\nU2 rockets total cost: " + U2TotalCost + "\nU2 rockets used: " + U2RocketsUsed + "\nMinimum U2 rockets needed: " + this.U2Rockets.size();
+        return result;
+    }
+
+    public ArrayList<Item> getItems()
+    {
+        return items;
+    }
+
+    public ArrayList<U1> getU1Rockets()
+    {
+        return U1Rockets;
+    }
+
+    public ArrayList<U2> getU2Rockets()
+    {
+        return U2Rockets;
     }
 }
